333e5d46a9327db7ed013e8267d04defb4929710
fernandopaz1 <ferpaz9292@gmail.com>
2021-07-30 23:18:30 -0300

    fix: para que pudiera instalar las dependencias de jdbc tenia que haber un unico entrypoint

--- src/main/java/com/example/accessingdatamysaql/MainController.java
+++ src/main/java/com/example/accessingdatamysaql/MainController.java
@@ -20,7 +20,7 @@ public class MainController {
       , @RequestParam String email) {
     // @ResponseBody means the returned String is the response, not a view name
     // @RequestParam means it is a parameter from the GET or POST request
-
+    System.out.println("Adding new user: " + name + " " + email);
     User n = new User();
     n.setName(name);
     n.setEmail(email);
